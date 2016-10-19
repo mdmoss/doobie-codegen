829d04f578e34258b4aef93e733982c2593a39ec
Matthew Moss <matthew@premonition.io>
2016-10-19 12:08:11 +1100

    Remove unnused NoInsert

--- src/main/scala/mdmoss/doobiegen/GenOptions.scala
+++ src/main/scala/mdmoss/doobiegen/GenOptions.scala
@@ -5,6 +5,5 @@ object GenOptions {
   sealed trait GenOption
 
   case object NoWrite                      extends GenOption
-  case object NoInsert                     extends GenOption
   case class ScalaDefault(default: String) extends GenOption
 }
