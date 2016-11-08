cd742a7eb4574bc5f02b3637fc56f2b8c53b0389
Matthew Moss <matthew@premonition.io>
2016-11-08 15:25:24 +1100

    More fixing

--- sql/make-test.sh
+++ sql/make-test.sh
@@ -5,6 +5,6 @@ IFS=$'\n\t'
 
 cd "$(dirname ${BASH_SOURCE[0]})"
 
-psql -U postgres -c 'DROP DATABASE gen'
+psql -U postgres -c 'DROP DATABASE IF EXISTS gen'
 psql -U postgres -c 'CREATE DATABASE gen'
 cat test.sql | psql -U postgres gen
